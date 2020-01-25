ba3754c1aaacc024cbbfce6305818fdeab30294b
Timothy Choy <choyt2@mcmaster.ca>
2020-01-25 17:40:08 -0500

    Fix App.js error

--- foodcycle/src/App.js
+++ foodcycle/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 import Home from './components/Home'
-import Home from "./components/Home"
 import MapPage from "./components/MapPage"
 import StoreSetInfo from "./components/StoreSetInfo"
 
