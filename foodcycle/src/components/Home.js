63a077b2c67b60202f2ff87e59c4cf0b0499e1bf
Leon So <leon_so7@hotmail.com>
2020-01-25 15:08:22 -0500

    home

---
+++ foodcycle/src/components/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import BackgroundImg from '../assets/background1.png';
+import '../styles/Home.css';
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      results: {}
+    }
+  }
+
+  render() {
+    return (
+     <div class="Home">
+       <p> Hello </p>
+     </div>
+    );
+  }
+}
+
+export default Home;
