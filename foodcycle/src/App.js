90efa6005f6ab716f4526003b627d2220ef061d0
Joy Xiao <xiaoz18@mcmaster.ca>
2020-01-25 16:00:44 -0500

    added submit button

--- foodcycle/src/App.js
+++ foodcycle/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './styles/App.css';
 import Home from "./components/Home.js"
+import StoreSetInfo from "./components/StoreSetInfo"
 
 class App extends Component {
   constructor() {
@@ -14,7 +15,7 @@ class App extends Component {
   render() {
     return (
       // Open home screen
-      <Home/>
+      <StoreSetInfo />
     );
   }
 }
