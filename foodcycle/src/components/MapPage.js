b783a39a64a46d43aa2bb29cd0a68bc69836e8ab
Leon So <leon_so7@hotmail.com>
2020-01-25 21:59:09 -0500

    map

--- foodcycle/src/components/MapPage.js
+++ foodcycle/src/components/MapPage.js
@@ -7,7 +7,7 @@ import { Carousel } from 'react-responsive-carousel';
 import Chicken from '../assets/chicken.jpeg';
 import Muffin from '../assets/muffin.jpg';
 import Yogurt from '../assets/yogurt.jpg';
-
+import GreenIcon from '../assets/Green2.png';
 class MapPage extends Component {
   constructor() {
     super();
@@ -32,7 +32,7 @@ class MapPage extends Component {
 
   render() {
     return (
-      <div>
+      <div style={{width: "80%"}}>
         <Map
           google={this.props.google}
           zoom={15}
@@ -43,16 +43,23 @@ class MapPage extends Component {
         >
 
           <Marker
-            name={'Current location'} />
+            title={'Current Location'}
+            name={'You are here!'}
+             />
 
           <div className="modal">
             <Modal
-              style={{ color: 'White', width: "50%", height: "50%", borderRadius: 20, marginLeft: "25%", marginTop: "10%", borderColor: "black", borderwidth: 1 }}
+              style={{ color: 'White', width: "50%", height: "50%", borderRadius: 20, marginLeft: "25%", marginTop: "2%", borderColor: "black", borderwidth: 1 }}
               open={this.state.modal}
               onClose={this.handleClose}
             >
               <div>
-                <h2>Fortinos</h2>
+                <h2>Fortinos (Main St.)</h2>
+                <h4>Hours of Operation:</h4>
+                <p>Saturday	7a.m.–9p.m.</p>
+                <p>Sunday	7a.m.–9p.m.</p>
+                <p>Monday-Friday	7a.m.–9p.m.</p>
+                <h4>Items available:</h4>
                 <Carousel infiniteLoop={true} showThumbs={false}>
                   <div>
                     <img src={Chicken} style={{width: '50%', height: '50%'}}/>
@@ -74,6 +81,31 @@ class MapPage extends Component {
             name={'Fortinos (Main St.)'}
             position={{ lat: 43.2553, lng: -79.9289 }}
             onMouseover={this.onMouseoverMarker}
+            icon={{
+                url: GreenIcon
+              }}
+          />
+
+            <Marker
+                onClick={this.handleOpen}
+                title={'Snooty Fox'}
+                name={'Snooty Fox'}
+                position={{ lat: 43.2618, lng: -79.9052 }}
+                onMouseover={this.onMouseoverMarker}
+                icon={{
+                    url: GreenIcon
+                }}
+          />
+
+            <Marker
+                onClick={this.handleOpen}
+                title={'Boston Pizza'}
+                name={'Boston Pizza'}
+                position={{ lat: 43.2572, lng: -79.9277 }}
+                onMouseover={this.onMouseoverMarker}
+                icon={{
+                    url: GreenIcon
+                }}
           />
 
           <InfoWindow onClose={this.onInfoWindowClose}>
