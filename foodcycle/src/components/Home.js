6497647e65808000cc3af242e5a733852cc5f137
Leon So <leon_so7@hotmail.com>
2020-01-26 00:04:52 -0500

    Home page changes

--- foodcycle/src/components/Home.js
+++ foodcycle/src/components/Home.js
@@ -16,15 +16,17 @@ class Home extends Component {
     return (
     <div class="Home">
       <div className="homeText">
-        <img src={icon} width='10%'></img>
+        <img src={icon} width='14%'></img>
         <p style={{fontSize: '56px', fontWeight: 'bold', marginTop: '0em', marginBottom: '0.5em'}}>FoodCycle</p>
+        <p style={{fontSize: '20px', marginTop: '-1.2em', marginBottom: '1em'}}>A community with a focus on food sustainability and reducing food waste.</p>
+        <p style={{fontSize: '20px', marginTop: '-1em', marginBottom: '1em'}}>Join our network today and make a change in your local community.</p>
         <div className="homeButton">
-          <Button variant="outlined" style={{color: '#00733b', width: '11em', fontWeight: 'bold', fontSize: '30px', borderColor: '#00733b', borderWidth: '3px', borderRadius: 100}}
+          <Button variant="outlined" style={{color: '#00733b', width: '11em', fontWeight: 'bold', fontSize: '24px', borderColor: '#00733b', borderWidth: '3px', borderRadius: 100}}
           component={Link} to="/map">
             Find Locations
           </Button>
           <p className="spacing"></p>
-          <Button variant="outlined" style={{color: '#00733b', width: '11em', fontWeight: 'bold', fontSize: '30px', borderColor: '#00733b', borderWidth: '3px', borderRadius: 100}}
+          <Button variant="outlined" style={{color: '#00733b', width: '11em', fontWeight: 'bold', fontSize: '24px', borderColor: '#00733b', borderWidth: '3px', borderRadius: 100}}
           component={Link} to="/store">
             I'm a Vendor
           </Button>
